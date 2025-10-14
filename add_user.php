b4a0166e6f49d8b02226fb8f0ad878a8a25d6fce
mithridatem <mathieumithridate@adrar-formation.com>
2025-10-14 15:59:13 +0200

    feat : add preg_match + verif_password

--- add_user.php
+++ add_user.php
@@ -15,10 +15,16 @@ if (isset($_POST["submit"])) {
  * Méthode pour gérer l'ajout d'un utilisateur (formulaire)
  * @return string message de sortie
  */
-function add_user(): string {
+function add_user(): string
+{
     //Test si les champs sont vides
     if (empty($_POST["firstname"]) || empty($_POST["lastname"]) || empty($_POST["email"]) || empty($_POST["password"])) {
         return "Veuillez remplir les 4 champs";
+    }
+
+    //Test si les 2 mot de passe correspondent
+    if ($_POST["password"] != $_POST["verif_password"]) {
+        return "Les 2 mots de passe ne correspondent pas";
     }
 
     //Nettoyer les entrées
@@ -26,7 +32,11 @@ function add_user(): string {
         $_POST[$key] = sanitize($value);
     }
 
-    //Vérifier si l'email est valide
+    if (!preg_match('/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{12,}$/', $_POST["password"]) ) {
+        return "Le mot de passe doit contenir des maj, min, des chiffres et au moins 12 caractères de longueur";
+    }
+
+    //Vérifier si l'email est invalide
     if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
         return "L'email n'est pas valide";
     }
@@ -35,25 +45,27 @@ function add_user(): string {
     if (is_user_exists($_POST["email"])) {
         return "Cet email n'est pas utilisable";
     }
-    
+
     //hash du password
     $_POST["password"] = password_hash($_POST["password"], PASSWORD_DEFAULT);
-    
+
     //Ajout du compte en BDD
     try {
         save_user($_POST);
         return "Le compte " . $_POST["email"] . " a été ajouté avec succes";
-    } catch(Exception $e) {
+    } catch (Exception $e) {
         return $e->getMessage();
     }
 }
 ?>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter un utilisateur</title>
 </head>
+
 <body>
     <h2>Ajouter un utilisateur</h2>
     <form action="" method="post">
@@ -61,8 +73,10 @@ function add_user(): string {
         <input type="text" name="lastname" placeholder="Saisir votre nom">
         <input type="email" name="email" placeholder="Saisir votre email">
         <input type="password" name="password" placeholder="Saisir un mot de passe">
+        <input type="password" name="verif_password" placeholder="Répeter le mot de passe">
         <input type="submit" value="Ajouter" name="submit">
     </form>
-    <p><?= $result ?? ""?></p>
+    <p><?= $result ?? "" ?></p>
 </body>
+
 </html>
